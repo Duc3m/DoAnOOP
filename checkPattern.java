917d0f914a4d2a26b73e0d552956f38df1bca0cc
Quangdung090 <ttramquangdung@gmail.com>
2023-12-14 23:50:30 +0700

    them regex check ngay thang

--- checkPattern.java
+++ checkPattern.java
@@ -31,20 +31,6 @@ public class checkPattern {
         } while (true);
     }
 
-    public static int checkNgayThang(Scanner sc) {
-        int result;
-        Pattern intPattern = Pattern.compile("^\\d{1,2}$");
-        do {
-            String temp = sc.nextLine();
-            Matcher findMatch = intPattern.matcher(temp);
-            if (findMatch.find()) {
-                result = Integer.parseInt(temp);
-                return result;
-            }
-            System.out.print("phai co 1-2 so, vui long nhap lai: ");
-        } while (true);
-    }
-
     public static int checkNam(Scanner sc) {
         int result;
         Pattern intPattern = Pattern.compile("^\\d{4}$");
